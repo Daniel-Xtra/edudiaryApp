da5386a1e42995bb5fafb7b258a9e1dfd3d04164
Daniel <danielakanbi01@gmail.com>
2021-08-30 13:00:46 -0700

    adding register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -19,4 +20,7 @@ export class LoginComponent implements OnInit {
     password: new FormControl()
   });
   
+  createAcc(){
+    this.router.navigate(['/register'])
+  }
 }
